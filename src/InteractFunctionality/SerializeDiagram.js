b9c20b826ba7ef7ac6e797225d9e91953fb80fa8
SeanvdMeer <18538762+minisean@users.noreply.github.com>
2023-05-17 17:38:40 +0200

    Added download text file function

--- src/InteractFunctionality/SerializeDiagram.js
+++ src/InteractFunctionality/SerializeDiagram.js
@@ -2,6 +2,19 @@ import * as _ from 'lodash';
 import * as React from 'react';
 import { DemoButton, DemoWorkspaceWidget } from '../Widgets/DemoWorkspaceWidget';
 
+function downloadTextFile(text, filename) {
+	const element = document.createElement('a');
+	element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+	element.setAttribute('download', filename);
+  
+	element.style.display = 'none';
+	document.body.appendChild(element);
+  
+	element.click();
+  
+	document.body.removeChild(element);
+  }
+
 export class Serializer extends React.Component {
 
 	constructor(props) {
@@ -13,6 +26,8 @@ export class Serializer extends React.Component {
 		let { model } = this.props;
 		var str = JSON.stringify(model.serialize());
 		console.log(str);
+		const filename = 'example.txt';
+		downloadTextFile(str, filename);
 	}
 
 	render() {
