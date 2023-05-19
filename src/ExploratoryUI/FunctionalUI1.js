c9f3458b7f379302a19f5da55f52fab8864148f0
SeanvdMeer <18538762+MiniSean@users.noreply.github.com>
2023-05-19 11:34:23 +0200

    Updated separated workspace toolbar

--- src/ExploratoryUI/FunctionalUI1.js
+++ src/ExploratoryUI/FunctionalUI1.js
@@ -163,21 +163,7 @@ export class WorkspacePanel extends React.Component {
             </div>
 
             {/* Right Toolbar */}
-            <Collapse id="Workspace Collapsable" in={isExpanded} collapsedWidth={64} timeout="auto" unmountOnExit>
-            <AppBar id="Toolbar" position="absolute" style={{ right: 0, top: '10%', bottom: '20%', width: 64, zIndex: 2 }}>
-                <Toolbar id="Toolbar Container" style={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'flex-end' }}>
-                <IconButton color="inherit" aria-label="Copy">
-                    <ContentCopy />
-                </IconButton>
-                <IconButton color="inherit" aria-label="Delete">
-                    <Clear />
-                </IconButton>
-                <IconButton color="inherit" aria-label="Format">
-                    <Settings />
-                </IconButton>
-                </Toolbar>
-            </AppBar>
-            </Collapse>
+            <WorkspaceToolbar isExpanded={isExpanded}/>
 
             {/* Toggle Button */}
             <IconButton
@@ -196,6 +182,32 @@ export class WorkspacePanel extends React.Component {
 }
 WorkspacePanel.propTypes = {
     app: Application,
+};
+
+export class WorkspaceToolbar extends React.Component {
+
+  render() {
+    return (
+      <Collapse id="Workspace Collapsable" in={this.props.isExpanded} collapsedWidth={64} timeout="auto" unmountOnExit>
+      <AppBar id="Toolbar" position="absolute" style={{ right: 0, top: '10%', bottom: '20%', width: 64, zIndex: 2 }}>
+          <Toolbar id="Toolbar Container" style={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'flex-end' }}>
+          <IconButton color="inherit" aria-label="Copy">
+              <ContentCopy />
+          </IconButton>
+          <IconButton color="inherit" aria-label="Delete">
+              <Clear />
+          </IconButton>
+          <IconButton color="inherit" aria-label="Format">
+              <Settings />
+          </IconButton>
+          </Toolbar>
+      </AppBar>
+      </Collapse>
+    );
+  }
+}
+WorkspaceToolbar.propTypes = {
+  isExpanded: Boolean,
 };
 
 export class WorkspaceContent extends React.Component {
