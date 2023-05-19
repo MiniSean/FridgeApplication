799b37658d5cac35d32f61a3e13268946713392f
SeanvdMeer <18538762+MiniSean@users.noreply.github.com>
2023-05-19 18:20:21 +0200

    Added right-angle port model

--- src/CustomNode/DescriptiveNode/DesciptiveNodeModel.js
+++ src/CustomNode/DescriptiveNode/DesciptiveNodeModel.js
@@ -1,15 +1,103 @@
-import {NodeModel, DefaultNodeModel} from '@projectstorm/react-diagrams';
+// Design prompt:
+// Write a class that extends the DefaultNodeModel from projectstorm react-diagrams library that:
+// - Has two output ports 'Output-I' and 'Output-Q'.
+// - Both ports should create a RightAnglePortModel, which is an extension of the DefaultPortModel (also from react-diagrams), this model creates a RightAngleLinkModel when calling its createLinkModel method.
+// - Make sure the CustomNodeModel and RightAnglePortModel classes have the export key.
+// - Create the corresponding CustomNodeFactory and RightAngleLinkFactory and give a comment in the code on where to use these.
+// - Create the corresponding CustomNodeWidget and RightAngleLinkWidget and give a comment in the code on where to use these.
+import { 
+   NodeModel,
+   DefaultNodeModel,
+   DefaultPortModel, 
+   DefaultLinkModel, 
+   DiagramEngine,
+   DefaultNodeWidget,
+   DefaultLinkWidget,
+   DefaultNodeFactory,
+   DefaultLinkFactory,
+   RightAngleLinkModel ,
+} from '@projectstorm/react-diagrams';
+import * as React from 'react';
+import { AbstractReactFactory } from '@projectstorm/react-canvas-core';
 
-export class DescriptiveNodeModel extends DefaultNodeModel {
-	constructor(props) {
-		super(props, {
-      name: 'New Name',
-      color: 'rgb(0,192,100)'
-		});
-    this.addInPort('Channel-I', true);
-    this.addInPort('Channel-Q', false);
-    this.addInPort('DIO');
-    this.addOutPort('Channel-I');
-    this.addOutPort('Channel-Q');
-	}
+// Custom Models //
+
+export class CustomNodeModel extends DefaultNodeModel {
+   constructor() {
+      super({
+         type: 'custom-node',
+      });
+
+      // Create the output ports
+      const portI = this.addPort(new RightAnglePortModel({ type: 'right-angle-port', name: 'Output-I', maximumLinks: 1, canLinkPort: true }));
+      const portQ = this.addPort(new RightAnglePortModel({ type: 'right-angle-port', name: 'Output-Q', maximumLinks: 1, canLinkPort: true }));
+      
+      // Set the desired position of the ports
+      portI.setPosition('right');
+      portQ.setPosition('right');
+
+      // Set properties for all ports
+      Object.values(this.ports).forEach((port) => {
+         port.canLinkPort = true;
+         port.maximumLinkCount = 1;
+      });
+   }
+
+   // // Override the createLinkModel method to use RightAngleLinkModel for all ports
+   // createLinkModel() {
+   //    return new RightAngleLinkModel();
+   // }
 }
+
+export class RightAnglePortModel extends DefaultPortModel {
+  createLinkModel() {
+    return new RightAngleLinkModel();
+  }
+}
+
+// class RightAngleLinkModel extends DefaultLinkModel {
+//   constructor() {
+//     super({
+//       type: 'right-angle-link',
+//     });
+//   }
+// }
+
+// Custom Factories //
+
+export class CustomNodeFactory extends AbstractReactFactory {
+   constructor() {
+     super('custom-node');
+   }
+ 
+   generateModel(initialConfig) {
+     return new CustomNodeModel();
+   }
+ 
+   generateReactWidget(event) {
+     return <CustomNodeWidget engine={this.engine} node={event.model} />;}
+ }
+ 
+//  export class RightAngleLinkFactory extends AbstractReactFactory {
+//    constructor() {
+//      super('right-angle-link');
+//    }
+ 
+//    generateModel(initialConfig) {
+//      return new RightAngleLinkModel();
+//    }
+ 
+//    generateReactWidget(event) {
+//      return <RightAngleLinkWidget engine={this.engine} link={event.model} />;return React.createElement(RightAngleLinkWidget, { engine: this.engine, link: event.model });
+//    }
+//  }
+
+ // Custom Widgets //
+
+export class CustomNodeWidget extends DefaultNodeWidget {
+   // Custom implementation for rendering the node widget
+}
+ 
+//  export class RightAngleLinkWidget extends DefaultLinkWidget {
+//    // Custom implementation for rendering the link widget
+//  }
--- src/Widgets/ApplicationWidget.js
+++ src/Widgets/ApplicationWidget.js
@@ -1,20 +1,24 @@
 import createEngine, {
   DefaultDiagramState,
   DefaultNodeModel,
+  DefaultPortModel,
   DiagramModel,
   DiagramEngine,
   PortModelAlignment,
+  PathFindingLinkFactory,
+  RightAngleLinkFactory,
 } from '@projectstorm/react-diagrams';
 // import the custom models
 import { DiamondNodeModel } from '.././CustomNode/DiamondNodeModel';
 import { DiamondNodeFactory } from '.././CustomNode/DiamondNodeFactory';
 import { SimplePortFactory } from '.././CustomNode/SimplePortFactory';
 import { DiamondPortModel } from '.././CustomNode/DiamondPortModel';
-import { DescriptiveNodeModel } from '../CustomNode/DescriptiveNode/DesciptiveNodeModel';
+import { CustomNodeModel, CustomNodeFactory } from '../CustomNode/DescriptiveNode/DesciptiveNodeModel';
 
 export class Application {
 	diagramModel;
 	diagramEngine;
+	pathFinding;
 
 	constructor() {
 		this.diagramEngine = this.getEngine();  // SRD.default();
@@ -24,6 +28,8 @@ export class Application {
 		if (state instanceof DefaultDiagramState) {
 			state.dragNewLink.config.allowLooseLinks = false;
 		}
+		// Smart link routing
+		this.pathfinding = this.diagramEngine.getLinkFactories().getFactory<PathFindingLinkFactory>(PathFindingLinkFactory.NAME);
 		// ############################################ MAGIC HAPPENS HERE
 
 		this.diagramModel = this.getModel();
@@ -35,10 +41,17 @@ export class Application {
 	getEngine() {
 		//1) setup the diagram engine
 		var engine = createEngine();
+		// var engine = new DiagramEngine();
 
 		// register other factories as well
 		engine.getPortFactories().registerFactory(new SimplePortFactory('diamond', (config) => new DiamondPortModel(PortModelAlignment.LEFT)));
 		engine.getNodeFactories().registerFactory(new DiamondNodeFactory());
+		// Smart link routing
+		// const pathfinding = engine.getLinkFactories().getFactory<PathFindingLinkFactory>(PathFindingLinkFactory.NAME);
+		// Right angle link factory
+		engine.getLinkFactories().registerFactory(new RightAngleLinkFactory(), 'right-angle-link');
+		// Custom node factory
+		engine.getNodeFactories().registerFactory(new CustomNodeFactory());
 
 		return engine;
 	}
@@ -62,11 +75,11 @@ export class Application {
 		node2.setPosition(400, 100);
 
 		//3-C) link the 2 nodes together
-		// var link1 = port1.link(port2);
+		var link1 = port1.link(port2, this.pathFinding);
 
 		//3-D) create an orphaned node
 		var node3 = new DefaultNodeModel('Node 3', 'rgb(0,192,255)');
-		node3.addOutPort('Out');
+		// node3.addPort(new RightAnglePortModel(true, 'in-1', 'In'));
 		node3.addOutPort('I')
 		node3.addOutPort('Q')
 		node3.setPosition(100, 200);
@@ -75,11 +88,11 @@ export class Application {
 		var node4 = new DiamondNodeModel('Mixer');
 		node4.setPosition(200, 200);
 
-		var node6 = new DescriptiveNodeModel('NodeName');
+		var node6 = new CustomNodeModel('NodeName');
 		node6.setPosition(400, 200)
 
 		//4) add the models to the root graph
-		model.addAll(node1, node2, node3, node4, node5, node6);
+		model.addAll(node1, node2, node3, node4, node5, node6, link1);
 
 		return model
 	}
