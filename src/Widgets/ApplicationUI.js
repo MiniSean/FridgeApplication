3d0220b474d50b3aabb13f73cb9da856ed838a8a
SeanvdMeer <18538762+MiniSean@users.noreply.github.com>
2023-05-31 23:21:11 +0200

    Added wip functionality for exporting canvas to pdf

--- src/Widgets/ApplicationUI.js
+++ src/Widgets/ApplicationUI.js
@@ -1,3 +1,50 @@
+// Design prompt (pdf export):
+// Respond as if you are a senior react application developer.
+// You are knowledgable about the react-diagram library, mui-material library, javascript and html.
+// When writing code you make sure the functions and comments are a little verbose to explain what you are doing.
+// After writing the code, give a short explanation of how to use the code.
+// Be sure to include the necessary imports for each code snippet.
+// Make sure all functions are present in their respective libraries or specify them yourself.
+
+// For context: 
+// - The project is about creating an application in which the user can make a custom diagram based on nodes and edges.
+// - The standard way of describing an object in the react-diagram library is through the use of a model class and a widget class.
+// The model class describes functionalities like serialization, while the widget class describes the html (or react.component) visualization.
+// - In order for the engine to understand how to construct these classes, we make use of factory classes that tell the engine how to construct individual nodes, ports and edges.
+
+// This provides the desired functionality (see code at the end of this message).
+// Lets try and finetune it a little bit.
+// The next assignment is to adjust the way the image is added to the pdfDoc:
+// - The workspace hosts a collection of nodes. Calculate a bounding box coordinates such that all nodes are within this bounding box.
+// - Ensure that if this bounding box is too large, the image is subdivided to fit on multiple pdf pages such that they can be placed side by side to create the original image.
+// - For the 'getBoundingBox' method, make use of the .getBoundingBox() method of a node. this array of 4 point (x and y) coordinates.
+
+// ```
+// import { saveAs } from 'file-saver';
+// import { toPng } from 'dom-to-image';
+// import jsPDF from 'jspdf';
+
+// handleExport = () => {
+//   const workspaceElement = document.getElementById('Workspace Content'); // Replace 'Workspace Content' with the ID of the wrapper element for the workspace
+
+//   // Create a new jsPDF instance
+//   const pdfDoc = new jsPDF();
+
+//   // Convert the workspace element to an image
+//   toPng(workspaceElement)
+//     .then((dataUrl) => {
+//       // Add the image to the PDF document
+//       pdfDoc.addImage(dataUrl, 'PNG', 0, 0);
+
+//       // Save the PDF file
+//       pdfDoc.save('diagram.pdf');
+//     })
+//     .catch((error) => {
+//       console.error('Error exporting diagram as PDF:', error);
+//     });
+// };
+// ```
+
 import React, { useContext, useState } from 'react';
 // import { remote } from 'electron';
 // import { writeFile as fsWriteFile, readFile as fsReadFile } from 'fs/promises';
@@ -6,6 +53,9 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Settings, Download, Upload, NoteAdd, DataObject, Folder, ToggleOnOutlined, ToggleOff, ContentCopy, Clear } from '@mui/icons-material';
 import { experimentalStyled } from '@mui/material/styles';
 import _ from 'lodash';
+import { saveAs } from 'file-saver';
+import jsPDF from 'jspdf';
+import { toPng } from 'dom-to-image';
 import { CanvasWidget } from '@projectstorm/react-canvas-core';
 import { TrayItemWidget } from './DragAndDropMenu/TrayItemWidget';
 import { DemoCanvasWidget } from './CanvasWidget';
@@ -247,7 +297,6 @@ export class WorkspaceToolbar extends React.Component {
     const engine = this.props.app.getDiagramEngine();
     const model = engine.getModel();
     const serializedModel = JSON.stringify(model.serialize());
-    console.log(serializedModel);
   
     const element = document.createElement('a');
     element.setAttribute('href', 'data:text/json;charset=utf-8,' + encodeURIComponent(serializedModel));
@@ -320,6 +369,94 @@ export class WorkspaceToolbar extends React.Component {
     console.log('Model cleared successfully!');
   };
 
+  // This helper function calculates the bounding box for all nodes
+  getBoundingBox(nodes) {
+    const coordinates = nodes.map(node => {
+      // bounding box returns points property with [0: bottom-left, 1: bottom-right, 2: top-right, 3: top-left]
+      const rectBox = node.getBoundingBox();  
+      const topLeftPoint = rectBox.points[3];
+      const bottomRightPoint = rectBox.points[1];
+      return { x: topLeftPoint.x, y: topLeftPoint.y, x2: bottomRightPoint.x, y2: bottomRightPoint.y };
+    });
+
+    return {
+      x: Math.min(...coordinates.map(coordinate => coordinate.x)),
+      y: Math.min(...coordinates.map(coordinate => coordinate.y)),
+      x2: Math.max(...coordinates.map(coordinate => coordinate.x2)),
+      y2: Math.max(...coordinates.map(coordinate => coordinate.y2))
+    };
+  }
+  
+  // This function takes the bounding box and returns an array of page coordinates
+  subdivideBoundingBox(bbox, pageWidth, pageHeight) {
+    const pages = [];
+    for (let x = bbox.x; x < bbox.x2 || x === bbox.x; x += pageWidth) {
+      for (let y = bbox.y; y < bbox.y2 || y === bbox.y; y += pageHeight) {
+        pages.push({ x, y, x2: Math.min(x + pageWidth, bbox.x2), y2: Math.min(y + pageHeight, bbox.y2) });
+      }
+    }
+    return pages;
+  }
+
+  /**
+   * Handle the pdf-export button click.
+   * Serializes the model and prompts the user to save the pdf to a file.
+   */
+  handleExportPDF = () => {
+    const engine = this.props.app.getDiagramEngine();
+    const workspaceElement = document.getElementById('Workspace Content'); // Replace 'Workspace Content' with the ID of the wrapper element for the workspace
+    const nodes = engine.getModel().getNodes();
+    const bbox = this.getBoundingBox(nodes);
+
+    const pdfDoc = new jsPDF('p', 'pt', 'a4');
+    const pdfPageWidth = pdfDoc.internal.pageSize.getWidth();
+    const pdfPageHeight = pdfDoc.internal.pageSize.getHeight();
+
+    // Convert pixels to points for jspdf (72 points = 1 inch, 96 pixels = 1 inch)
+    const pixelToPointsRatio = 72 / 96;
+    const pageWidth = pdfPageWidth / pixelToPointsRatio;
+    const pageHeight = pdfPageHeight / pixelToPointsRatio;
+
+    const pages = this.subdivideBoundingBox(bbox, pageWidth, pageHeight);
+    console.log('Reached here: ', nodes, bbox, pageWidth, pageHeight, pages);
+
+    // let pageCount = 0;
+    // pages.forEach((page) => {
+    //   if (pageCount > 0) {
+    //     pdfDoc.addPage();
+    //   }
+    //   console.log('This is a page: ', page);
+      
+    //   // const clone = workspaceElement.cloneNode(true);
+    //   const clone = workspaceElement;
+    //   clone.style.transform = `translate(${-page.x}px, ${-page.y}px)`;
+    //   toPng(clone, {
+    //     width: page.x2 - page.x,
+    //     height: page.y2 - page.y
+    //   }).then((dataUrl) => {
+    //     pdfDoc.addImage(dataUrl, 'PNG', 0, 0);
+    //     pageCount++;
+    //     if (pageCount === pages.length) {
+    //       pdfDoc.save('diagram.pdf');
+    //     }
+    //   }).catch((error) => {
+    //     console.error('Error exporting diagram as PDF:', error);
+    //   });
+    // });
+
+    // Convert the workspace element to an image
+    toPng(workspaceElement).then((dataUrl) => {
+        // Add the image to the PDF document
+        pdfDoc.addImage(dataUrl, 'svg', 0, 0);
+
+        // Save the PDF file
+        pdfDoc.save('diagram.pdf');
+    })
+    .catch((error) => {
+      console.error('Error exporting diagram as PDF:', error);
+    });
+  };
+
   render() {
     const { openDialog } = this.context;
 
@@ -341,6 +478,9 @@ export class WorkspaceToolbar extends React.Component {
           </IconButton>
           <IconButton color="inherit" aria-label="Copy" title="Config" onClick={openDialog}>
               <DataObject />
+          </IconButton>
+          <IconButton color="inherit" aria-label="Copy" title="Export to PDF" onClick={this.handleExportPDF}>
+              <Upload />
           </IconButton>
           <IconButton color="inherit" aria-label="Delete" title="Delete">
               <Clear />
